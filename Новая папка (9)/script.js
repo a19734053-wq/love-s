0d94df5e72ba3d02ce0e7bbc5416d91e1ec15e9a
a19734053-wq <a19734053@gmail.com>
2026-02-16 18:10:49 +0600

    Add files via upload

---
+++ Новая папка (9)/script.js
@@ -0,0 +1,14 @@
+//Массив набор упорядоченных данных
+const arr = ['Alihan', 'Bekzat', 'Alisher',
+    'Rima'
+    
+
+
+]
+
+console.log(arr)
+console.log(arr[0])
+console.log(arr.length)
+console.log(arr[arr.length - 1])
+
+
